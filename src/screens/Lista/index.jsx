d3ed002a3e3b4201cd1de1938c6b95757af8862b
Hyago Rodrigues <hyago.andrade@meliuz.com.br>
2022-01-24 16:22:21 -0300

    feat: add btn

--- src/screens/Lista/index.jsx
+++ src/screens/Lista/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Text, StatusBar } from 'react-native';
 import { Container } from '../../components/Container';
-
+import { NewProduct } from '../../components/Botões'
+import { FontAwesome } from '@expo/vector-icons';
 const Lista = () => {
     return (
         <Container>
             <StatusBar />
             <Text>LISTA DE COMPRAS</Text>
+            <NewProduct>
+            <FontAwesome
+                name='plus'
+                style={{
+                    marginHorizontal: 20
+                }}
+                size={15}
+                color={'#fff'}
+                onPress={() => console.log('hello')} />
+
+            </NewProduct>
         </Container>
     );
 };
