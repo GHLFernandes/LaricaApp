14676d6dd703c909529e9ed980577f0edfb0e412
Hyago Rodrigues <hyago.andrade@meliuz.com.br>
2022-01-25 16:18:56 -0300

    feat: estilização condicional

--- src/components/Estilos/index.jsx
+++ src/components/Estilos/index.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Dimensions } from 'react-native';
-import { corPrimaria, corPrimariaClaro } from '../../components/UI/variaveis';
+import { corPrimaria, corPrimariaClaro, fundoClaro, fundoEscuro } from '../../components/UI/variaveis';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
@@ -32,16 +32,29 @@ export const InputListContainer = styled.View.attrs({
 })``
 
 export const TitleListContainer = styled.View.attrs({
-    backgroundColor: corPrimariaClaro,
+    backgroundColor: fundoEscuro,
     height: 50,
     alignItems: 'center',
     justifyContent: 'center',
-    width: '100%'
+    width: '100%',
     
 })``
 
+export const ItemsContainer = styled.View.attrs({
+    flexDirection: 'row',
+    height: 50,
+    backgroundColor: fundoClaro,
+    marginVertical: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 15
+})``
 
 //Área de botões ------------------------------
+
+export const ItemBtn = styled.TouchableOpacity.attrs({
+    flex: 1
+})``
 
 export const ListAddBtn = styled.TouchableOpacity.attrs({
     width: '20%',
@@ -49,5 +62,18 @@ export const ListAddBtn = styled.TouchableOpacity.attrs({
 
 })``
 
-// Área de Textos ------------------------------
+export const DelBtn = styled.TouchableOpacity`
+    padding-right: 15px;
+    padding-left: 15px;
+`
 
+// Área de Textos ------------------------------
+export const Title = styled.Text`
+    font-size: 20px;
+    color: white;
+`
+export const ItemChecked = styled.Text`
+  text-decoration: ${props => props.check ? "line-through" : "none"};
+  font-size: 20px;
+  color: white;
+`
--- src/screens/Lista/index.jsx
+++ src/screens/Lista/index.jsx
@@ -1,10 +1,11 @@
 
 import React, { useState } from 'react';
-import { SafeAreaView, View, Text, TextInput, TouchableOpacity, FlatList, StatusBar, StyleSheet } from 'react-native';
-import { corPrimaria, corPrimariaClaro, fundoClaro } from '../../components/UI/variaveis';
-import {InputList, MainListContainer, InputListContainer, TitleListContainer, ListAddBtn, TextAddBtn} from '../../components/Estilos/index';
+import { FlatList, StatusBar} from 'react-native';
+import { corPrimaria} from '../../components/UI/variaveis';
+import {InputList, MainListContainer, InputListContainer, TitleListContainer, ListAddBtn, ItemsContainer, Title, ItemBtn, DelBtn, ItemChecked} from '../../components/Estilos/index';
 import { FontAwesome } from '@expo/vector-icons';
 import useToDoList from '../../hooks/useListaHook';
+import { isTryStatement } from 'typescript';
 
 const Lista = () => {
     const [produto, setProduto] = useState('');
@@ -14,7 +15,7 @@ const Lista = () => {
             <StatusBar
                 barStyle='dark-content'
             />
-            <TitleListContainer><Text>LISTA DE COMPRAS</Text></TitleListContainer>            
+            <TitleListContainer><Title>LISTA DE COMPRAS</Title></TitleListContainer>            
             <InputListContainer >                
                 <InputList
                    placeholder="Adicionar Produto"
@@ -34,69 +35,21 @@ const Lista = () => {
             <FlatList
                 data={state}
                 renderItem={({ item }) => (
-                    <View style={estilo.itemsContainer}>
-                        <TouchableOpacity style={estilo.itemBtn}
+                    <ItemsContainer>
+                        <ItemBtn
                             onPress={() => { check(item.id) }}>
-                            <Text style={[estilo.listItem, item.check ? estilo.listItemChecked : '']} >{item.title}</Text>
-                        </TouchableOpacity>
+                            <ItemChecked check={item.check}>{item.title}</ItemChecked>
+                        </ItemBtn>
 
-                        <TouchableOpacity style={estilo.delBtn}
+                        <DelBtn 
                             onPress={() => { del(item.id) }}>
                             <FontAwesome name="trash-o" size={25} color="red" />
-                        </TouchableOpacity>
-                    </View>
+                        </DelBtn>
+                    </ItemsContainer>
                 )
                 } />
         </MainListContainer>
     )
 }
 
-const estilo = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        margin: 10,
-    },
-    input: {
-        flex: 1,
-        width: '80%',
-        fontSize: 25,
-        color: 'black'
-    },
-    addBtn: {
-        width: '20%',
-        alignItems: 'center',
-        alignSelf: 'center'
-    },
-    addBtnTxt: {
-        alignItems: 'center',
-        alignSelf: 'center',
-        color: 'green',
-        fontSize: 50
-    },
-    itemsContainer: {
-        flexDirection: 'row',
-        backgroundColor: fundoClaro
-    },
-    itemBtn: {
-        flex: 1,
-    },
-    listItem: {
-        fontSize: 22,
-        color: "white",
-        marginVertical: 3,
-        marginHorizontal: 7,
-        padding: 10,
-    },
-    listItemChecked: {
-        textDecorationLine: 'line-through'
-    },
-    delBtn: {
-        alignItems: 'center',
-        alignSelf: 'center',
-        paddingHorizontal: 15
-    },
-})
 export default Lista;
