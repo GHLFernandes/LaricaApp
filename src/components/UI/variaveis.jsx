065a389c90e3f260cd2c2c22c855350747f89bc7
Gabriel Fernandes <64507673+GHLFernandes@users.noreply.github.com>
2022-01-26 10:36:09 -0300

    Revert "Lista de compras"

--- src/components/UI/variaveis.jsx
+++ src/components/UI/variaveis.jsx
@@ -1,5 +1,4 @@
 export const corPrimaria = '#E19046';
 export const corPrimariaClaro = '#E8C7AB';
-export const fundoClaro = '#ff9e00';
-export const fundoEscuro = '#ff6d00';
+export const fundoClaro = '';
 export const textoFundoClaro = '';
--- src/screens/Lista/index.jsx
+++ src/screens/Lista/index.jsx
@@ -1,55 +1,101 @@
 
 import React, { useState } from 'react';
-import { FlatList, StatusBar} from 'react-native';
-import { corPrimaria} from '../../components/UI/variaveis';
-import {InputList, MainListContainer, InputListContainer, TitleListContainer, ListAddBtn, ItemsContainer, Title, ItemBtn, DelBtn, ItemChecked} from '../../components/Estilos/index';
+import { SafeAreaView, View, Text, TextInput, TouchableOpacity, FlatList, StatusBar, StyleSheet } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import useToDoList from '../../hooks/useListaHook';
-import { isTryStatement } from 'typescript';
 
 const Lista = () => {
     const [produto, setProduto] = useState('');
     const [state, add, check, del] = useToDoList();
+
     return (
-        <MainListContainer>
+        <SafeAreaView style={estilo.container}>
             <StatusBar
                 barStyle='dark-content'
             />
-            <TitleListContainer><Title>LISTA DE COMPRAS</Title></TitleListContainer>            
-            <InputListContainer >                
-                <InputList
-                   placeholder="Adicionar Produto"
-                   placeholderTextColor={'black'}
-                   value={produto}
-                   onChangeText={text => setProduto(text)}
-                 />
-                <ListAddBtn
+            <Text>LISTA DE COMPRAS</Text>
+            <View style={estilo.inputContainer}>
+                <TextInput style={estilo.input}
+                    placeholder="Adicionar Produto"
+                    placeholderTextColor={'black'}
+                    value={produto}
+                    onChangeText={text => setProduto(text)}
+                />
+                <TouchableOpacity style={estilo.addBtn}
                     onPress={() => {
                         add(produto);
                         setProduto('');
                     }} >
-                    <FontAwesome name="plus" size={30} color={corPrimaria} />
-                </ListAddBtn>
-            </InputListContainer>
+                    <Text style={estilo.addBtnTxt}>+</Text>
+                </TouchableOpacity>
+            </View>
 
             <FlatList
                 data={state}
                 renderItem={({ item }) => (
-                    <ItemsContainer>
-                        <ItemBtn
+                    <View style={estilo.itemsContainer}>
+                        <TouchableOpacity style={estilo.itemBtn}
                             onPress={() => { check(item.id) }}>
-                            <ItemChecked check={item.check}>{item.title}</ItemChecked>
-                        </ItemBtn>
+                            <Text style={[estilo.listItem, item.check ? estilo.listItemChecked : '']} >{item.title}</Text>
+                        </TouchableOpacity>
 
-                        <DelBtn 
+                        <TouchableOpacity style={estilo.delBtn}
                             onPress={() => { del(item.id) }}>
                             <FontAwesome name="trash-o" size={25} color="red" />
-                        </DelBtn>
-                    </ItemsContainer>
+                        </TouchableOpacity>
+                    </View>
                 )
                 } />
-        </MainListContainer>
+        </SafeAreaView >
     )
 }
 
+const estilo = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        margin: 10,
+    },
+    input: {
+        flex: 1,
+        width: '80%',
+        fontSize: 25,
+        color: 'black'
+    },
+    addBtn: {
+        width: '20%',
+        marginHorizontal: 2,
+        alignItems: 'center',
+        alignSelf: 'center'
+    },
+    addBtnTxt: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        color: 'green',
+        fontSize: 50
+    },
+    itemsContainer: {
+        flexDirection: 'row',
+    },
+    itemBtn: {
+        flex: 1,
+    },
+    listItem: {
+        fontSize: 22,
+        color: "black",
+        marginVertical: 3,
+        marginHorizontal: 7,
+        padding: 10,
+    },
+    listItemChecked: {
+        textDecorationLine: 'line-through'
+    },
+    delBtn: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        paddingHorizontal: 15
+    },
+})
 export default Lista;
